8b25c883c59c456b63b60e7b188248be2b3a866d
Martin Burolla <mburolla@xpanxion.com>
2021-12-13 14:41:51 -0500

    update

---
+++ readme.md
@@ -0,0 +1,4 @@
+# SpringBoot Data Access
+
+# Endpoints
+GET localhost:8080/instructor/api/v1/message
---
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/InstructorController.java
@@ -0,0 +1,13 @@
+package com.xpanxion.java.springboot.da1.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InstructorController {
+
+    @GetMapping("instructor/api/v1/message")
+    public String getMessage() {
+        return "Hello world.";
+    }
+}
