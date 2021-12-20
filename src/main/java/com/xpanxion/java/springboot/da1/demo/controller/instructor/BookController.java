3fb991df3097d76086d3d5f8a3e84b0dbe84025a
Martin Burolla <mburolla@xpanxion.com>
2021-12-19 19:27:05 -0500

    tidy

--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/instructor/BookController.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/instructor/BookController.java
@@ -1,21 +1,6 @@
 package com.xpanxion.java.springboot.da1.demo.controller.instructor;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Objects;
-
-import com.xpanxion.java.springboot.da1.demo.model.instructor.Book;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.transaction.Transactional;
 
 @RestController
 public class BookController {
--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/instructor/ProductController.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/instructor/ProductController.java
@@ -1,15 +1,8 @@
 package com.xpanxion.java.springboot.da1.demo.controller.instructor;
 
-import com.xpanxion.java.springboot.da1.demo.model.instructor.Product;
-import com.xpanxion.java.springboot.da1.demo.service.instructor.DataService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class ProductController {
-
 
 }
