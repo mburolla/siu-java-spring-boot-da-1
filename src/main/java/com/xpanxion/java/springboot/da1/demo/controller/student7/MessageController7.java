bb93adf22cc8fbd37bc7b4c9b8ec58b8d25cc275
Austin King <austintriesemail@gmail.com>
2021-12-20 13:48:00 -0600

    Update Message Controller

--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student7/MessageController7.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student7/MessageController7.java
@@ -8,6 +8,6 @@ public class MessageController7 {
 
     @GetMapping("student7/api/v1/message")
     public String getMessage() {
-        return "My name is Austin King";
+        return "My name is Austin H.W. King";
     }
 }
