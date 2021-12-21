3fc5cf5ccd6ccd69f96b64a54e44ef84fda61873
Elizabeth Michael <lisakada@gmail.com>
2021-12-21 14:18:41 -0600

    Finish Ex.7

--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student4/BookController4.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student4/BookController4.java
@@ -1,20 +1,38 @@
 package com.xpanxion.java.springboot.da1.demo.controller.student4;
 
 import com.xpanxion.java.springboot.da1.demo.model.student4.Book;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
-
+import java.util.List;
 
 @RestController
-    public class BookController4 {
+public class BookController4 {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private final String SELECT_BOOK = "select * from book where book_id = ?";
 
     @GetMapping("student4/api/v1/book")
-        public Book getBook() {
-            Book book = new Book(1,"Java",1234,34.56);
-            return book;
-
-        }
+    public List<Book> getBook() {
+        int bookId = 1;
+        List<Book> bookList;
+        bookList = jdbcTemplate.query(SELECT_BOOK, (row, rowNum) -> {
+            int id = Integer.parseInt(row.getString("book_id"));
+            String title = row.getString("title");
+            String isbn = row.getString("isbn");
+            Double price = Double.parseDouble(row.getString("price"));
+            return new Book(id, title, isbn, price);
+        }, bookId);
+        return bookList;
     }
+}
+
+
 
