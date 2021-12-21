f2a49640d84612ef821f0ffd31fd4ad3b9981ed6
Rachel Eckels <Rachel.Eckels@walmart.com>
2021-12-21 11:51:53 -0600

    Finish Ex7 and Update isbn for Book

--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student10/BookController10.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student10/BookController10.java
@@ -1,16 +1,45 @@
 package com.xpanxion.java.springboot.da1.demo.controller.student10;
 
-
+import org.springframework.stereotype.Service;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import com.xpanxion.java.springboot.da1.demo.model.student10.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Service
 @RestController
 public class BookController10 {
 
-    @GetMapping("student10/api/v1/book")
-    public Book getBook() {
-        return new Book(1, "A Brief History of Time", 20.00);
-    }
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    private final String SELECT_BOOK = "select * from book where book_id = ?";
 
+    /*
+    @GetMapping("student10/api/v1/book")
+    public List<Person> getPerson(Integer personId) {
+        List<Person> personList;
+        personList = jdbcTemplate.query(SELECT_PERSON, (row, rowNum) -> {
+            var id = Integer.parseInt(row.getString("id"));
+            var name =  row.getString("firstname");
+            return new Person(id, name);
+        }, personId);
+        return personList;
+    }
+    */
+
+    @GetMapping("student10/api/v1/book")
+    public List<Book> getBook(){
+        List<Book> bookList;
+        bookList = jdbcTemplate.query(SELECT_BOOK, (row, rowNum) -> {
+            var id = Integer.parseInt(row.getString("book_id"));
+            var title =  row.getString("title");
+            var isbn = row.getString("isbn");
+            var price = Double.parseDouble(row.getString("price"));
+            return new Book(id, title, isbn, price);
+        }, 1);
+        return bookList;
+    }
 }
