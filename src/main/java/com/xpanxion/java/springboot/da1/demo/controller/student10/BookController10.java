b305e90b73461ebb36b37e0b69b25c1a430a0961
Rachel Eckels <Rachel.Eckels@walmart.com>
2021-12-21 13:14:23 -0600

    Deleted @Service

--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student10/BookController10.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student10/BookController10.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.xpanxion.java.springboot.da1.demo.model.student10.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
-@Service
 @RestController
 public class BookController10 {
 
