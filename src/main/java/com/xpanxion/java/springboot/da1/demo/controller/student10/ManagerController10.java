15199d38904466f94da50e0f16695c73ea610c25
Rachel Eckels <Rachel.Eckels@walmart.com>
2021-12-22 14:54:15 -0600

    Deleted unnecessary imports

--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student10/ManagerController10.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student10/ManagerController10.java
@@ -3,8 +3,6 @@ package com.xpanxion.java.springboot.da1.demo.controller.student10;
 import com.xpanxion.java.springboot.da1.demo.model.student10.Manager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
