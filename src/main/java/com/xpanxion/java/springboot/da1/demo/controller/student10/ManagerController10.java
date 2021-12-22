fe60957230e26f94ba81b8ccec1e3423b4f67793
Rachel Eckels <Rachel.Eckels@walmart.com>
2021-12-22 14:51:43 -0600

    Finish Ex9

--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student10/ManagerController10.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student10/ManagerController10.java
@@ -3,7 +3,11 @@ package com.xpanxion.java.springboot.da1.demo.controller.student10;
 import com.xpanxion.java.springboot.da1.demo.model.student10.Manager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,6 +18,8 @@ public class ManagerController10 {
     @Autowired
     private JdbcTemplate jdbcTemplate;
     private final String SELECT_MANAGER = "select * from manager";
+    private final String INSERT_MANAGER = "insert into manager (full_name) values (?)";
+
 
     @GetMapping("student10/api/v1/managers")
     public List<Manager> getManagers () {
@@ -26,4 +32,9 @@ public class ManagerController10 {
         return managerList;
     }
 
+    @PostMapping("student10/api/v1/managers")
+    public Manager addManager (@RequestBody Manager manager) {
+        jdbcTemplate.update(INSERT_MANAGER, (manager.getFull_name()));
+        return manager;
+    }
 }
