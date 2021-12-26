0fe4bd5c26ae7975f4dd3afe059d4048ef6b38ef
Martin Burolla <mburolla@xpanxion.com>
2021-12-26 16:07:28 -0500

    Add manager

---
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/model/instructor/Manager.java
@@ -0,0 +1,28 @@
+package com.xpanxion.java.springboot.da1.demo.model.instructor;
+
+public class Manager {
+
+    private int id;
+    private String fullName;
+
+    public Manager(int id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
