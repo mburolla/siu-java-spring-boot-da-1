5071e68f472fe51f306e4f6e71fc95d383534526
Austin King <austintriesemail@gmail.com>
2021-12-27 15:25:54 -0600

    Create Book Endpoint

--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student7/BookController7.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student7/BookController7.java
@@ -3,8 +3,12 @@ package com.xpanxion.java.springboot.da1.demo.controller.student7;
 import com.xpanxion.java.springboot.da1.demo.model.student7.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,6 +17,9 @@ public class BookController7 {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
     private final String SELECT_BOOK = "select * from book where book_id = ?";
 
     @GetMapping("student7/api/v1/book")
@@ -27,6 +34,28 @@ public class BookController7 {
             return new Book(bookId, title, isbn, price);
         },numOfBook);
         return bookList;
+    }
+
+    @PostMapping("Student7/api/v1/bookstores/{bookstoreId}/books")
+    public void insertBook(@PathVariable int bookstoreId, @RequestBody Book Book) {
+        String INSERT_BOOK = "INSERT INTO book(title, isbn, price) Values (:title, :isbn, :price)";
+        String LINK_BOOKSTORE = "INSERT INTO bookstore_book(bookstore_id, book_id,quantity) VALUES (:bookstoreId,:bookId,:quantity)";
+        final KeyHolder keyId = new GeneratedKeyHolder();
+
+//        Creates book and grabs book_id
+        SqlParameterSource namedParameters = new MapSqlParameterSource("title", Book.getTitle())
+                .addValue("isbn", Book.getIsbn())
+                .addValue("price", Book.getPrice());
+        namedParameterJdbcTemplate.update(INSERT_BOOK, namedParameters,keyId);
+
+        SqlParameterSource bookstoreParameters = new MapSqlParameterSource("bookstoreId", bookstoreId)
+                .addValue("bookId", keyId.getKey().intValue())
+                .addValue("quantity", 1);
+        namedParameterJdbcTemplate.update(LINK_BOOKSTORE, bookstoreParameters);
+
+
+
+
     }
 }
 
