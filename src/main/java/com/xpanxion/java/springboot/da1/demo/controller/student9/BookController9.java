58df8c0ea3ae62439fd76a5637539b1a24fbdf68
EMANUEL GARCIA <e0g0186@HOMEOFFICE.wal-mart.com>
2021-12-27 14:55:03 -0600

    Ex12, Updated code to use keyholder

--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student9/BookController9.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student9/BookController9.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Key;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,7 +34,7 @@ public class BookController9 {
 }
 
 @Service
- class DataAccess9 {
+class DataAccess9 {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -52,13 +54,16 @@ public class BookController9 {
         params.put("title",book.getTitle());
         params.put("isbn",book.getIsbn());
         params.put("price", String.valueOf(book.getPrice()));
-        namedParameterJdbcTemplate.update(INSERT_BOOK,params);
 
-        var tempId = getBookId(book);
+        MapSqlParameterSource tempParams = new MapSqlParameterSource().addValues(params);
+        final KeyHolder holder = new GeneratedKeyHolder();
+        namedParameterJdbcTemplate.update(INSERT_BOOK, tempParams,holder, new String[]{"book_id"});
 
+
+        var tempId = Integer.parseInt(String.valueOf(holder.getKey()));
 
         bookstore_params.put("bookstore_id",bookstoreId);
-        bookstore_params.put("book_id",tempId);
+        bookstore_params.put("book_id", tempId);
         bookstore_params.put("quantity",quantity);
         namedParameterJdbcTemplate.update(LINK_BOOK,bookstore_params);
     }
