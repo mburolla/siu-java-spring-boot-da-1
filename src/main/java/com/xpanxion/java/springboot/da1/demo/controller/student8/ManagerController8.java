9050a29500a3410407c048d021c0b2576752ef5c
Meg <helloiammegg@gmail.com>
2021-12-28 15:56:43 -0600

    finish ex14

--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student8/ManagerController8.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student8/ManagerController8.java
@@ -1,9 +1,8 @@
 package com.xpanxion.java.springboot.da1.demo.controller.student8;
 
 import com.xpanxion.java.springboot.da1.demo.model.student8.Manager;
+import com.xpanxion.java.springboot.da1.demo.service.student8.DataService8;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,67 +11,22 @@ import java.util.List;
 public class ManagerController8 {
 
     @Autowired
-    private DataAccess8 dataAccess;
-
+    private DataService8 dataService8;
 
     @GetMapping("student8/api/v1/managers")
     public List<Manager> getManager() {
-        var managers = dataAccess.getAllManagers();
+        var managers = dataService8.getAllManagers();
         return managers;
     }
 
     @PostMapping("student8/api/v1/managers")
     public void createManager(@RequestBody Manager manager) {
-        dataAccess.createManager(manager);
+        dataService8.createManager(manager);
     }
-
 
     @PutMapping("student8/api/v1/managers")
     public void updateManager(@RequestBody Manager manager) {
-        dataAccess.updateManager(manager);
-    }
-
-
-
-    @Service
-    static class DataAccess8 {
-
-        // DATA
-
-        @Autowired
-        private JdbcTemplate jdbcTemplate;
-        private final String SELECT_All_MANAGERS = "select * from manager";
-        private final String ADD_A_MANAGER = "INSERT INTO MANAGER (full_name) VALUES (?)";
-        private final String UPDATE_A_MANAGER = "UPDATE MANAGER SET full_name = ? where manager_id = ?";
-
-
-        // CONSTRUCTOR
-
-        public DataAccess8() {
-
-        }
-
-        // METHODS
-
-        public List<Manager> getAllManagers() {
-            List<Manager> listOfManagers;
-            listOfManagers = jdbcTemplate.query(SELECT_All_MANAGERS, (row, rowNum) -> {
-                int id = Integer.parseInt(row.getString("manager_id"));
-                String name = row.getString("full_name");
-                return new Manager(id, name);
-            });
-            return listOfManagers;
-        }
-
-        public void createManager(Manager manager) {
-            System.out.println(manager);
-            jdbcTemplate.update(ADD_A_MANAGER, manager.getFullName());
-        }
-
-        public void updateManager(Manager manager) {
-            System.out.println(manager);
-            jdbcTemplate.update(UPDATE_A_MANAGER, manager.getFullName(), manager.getId());
-        }
+        dataService8.updateManager(manager);
     }
 
 }
