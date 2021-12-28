dbd592882f0d70f60d84afcce53b9cced247be94
Keith Inscore <keith.inscore@walmart.com>
2021-12-28 15:09:35 -0600

    finish ex14

--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student3/BookController3.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student3/BookController3.java
@@ -1,12 +1,8 @@
 package com.xpanxion.java.springboot.da1.demo.controller.student3;
 
 import com.xpanxion.java.springboot.da1.demo.model.student3.Book;
+import com.xpanxion.java.springboot.da1.demo.service.student3.DataService3;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,46 +11,16 @@ import java.util.List;
 public class BookController3 {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private DataService3 dataService3;
 
-    @Autowired
-    private NamedParameterJdbcTemplate template;
-
-    private final String SELECT_BOOK = "select * from book where book_id = ?";
-    private final String ADD_BOOK = "insert into book (title, isbn, price) values (:title, :isbn, :price)";
-    private final String LINK_BOOK_BOOKSTORE = "insert into bookstore_book (bookstore_id, book_id, quantity) values (:bookstore_id, :book_id, :quantity)";
 
     @GetMapping("student3/api/v1/book")
     public List<Book> getBook() {
-        int bookId = 1;
-        List<Book> bookList;
-        bookList = jdbcTemplate.query(SELECT_BOOK, (row, rowNum) ->{
-            var id = Integer.parseInt(row.getString("book_id"));
-            var title = row.getString("title");
-            var isbn = row.getString("isbn");
-            var price = Double.parseDouble(row.getString("price"));
-            return new Book(id, title, isbn, price);
-        }, bookId);
-        return bookList;
+        return dataService3.getBook();
     }
 
     @PostMapping("student3/api/v1/bookstores/{bookstoreId}/books")
     public Book addBook (@RequestBody Book book, @PathVariable Integer bookstoreId, @RequestParam int quantity){
-        final KeyHolder keyHolder = new GeneratedKeyHolder();
-        MapSqlParameterSource parameters = new MapSqlParameterSource()
-                .addValue("title", book.getTitle())
-                .addValue("isbn", book.getIsbn())
-                .addValue("price", book.getPrice());
-        template.update(ADD_BOOK, parameters, keyHolder);
-
-        var bookId = keyHolder.getKey().intValue();
-
-        MapSqlParameterSource bookstoreParameters = new MapSqlParameterSource()
-                .addValue("bookstore_id", bookstoreId)
-                .addValue("book_id", bookId)
-                .addValue("quantity", quantity);
-        template.update(LINK_BOOK_BOOKSTORE, bookstoreParameters);
-        book.setId(bookId);
-        return book;
+        return dataService3.addBook(book, bookstoreId, quantity);
     }
 }
--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student3/ManagerController3.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student3/ManagerController3.java
@@ -1,8 +1,8 @@
 package com.xpanxion.java.springboot.da1.demo.controller.student3;
 
 import com.xpanxion.java.springboot.da1.demo.model.student3.Manager;
+import com.xpanxion.java.springboot.da1.demo.service.student3.DataService3;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,31 +11,20 @@ import java.util.List;
 public class ManagerController3 {
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
-    private final  String SELECT_MANAGER = "select * from manager";
-    private final String INSERT_MANAGER = "insert into manager full_name values ?";
-    private final String UPDATE_MANAGER = "update manager set full_name = ? where manager_id = ?";
+    DataService3 dataService3;
 
     @GetMapping("student3/api/v1/managers")
     public List<Manager> getManager(){
-        List<Manager> managerList;
-        managerList = jdbcTemplate.query(SELECT_MANAGER, (row, rowNum) -> {
-                var id = Integer.parseInt(row.getString("manager_id"));
-                var fullName = row.getString("full_name");
-                return new Manager(id, fullName);
-            });
-        return managerList;
+        return dataService3.getManager();
     }
 
     @PostMapping("student3/api/v1/managers")
     public Manager addManager(@RequestBody Manager manager){
-        jdbcTemplate.update(INSERT_MANAGER, manager.getFullName());
-        return manager;
+        return dataService3.addManager(manager);
     }
 
     @PutMapping("student3/api/v1/managers")
     public Manager updateManagerName(@RequestBody Manager manager){
-        jdbcTemplate.update(UPDATE_MANAGER, manager.getFullName(), manager.getId());
-        return manager;
+        return dataService3.updateManagerName(manager);
     }
 }
