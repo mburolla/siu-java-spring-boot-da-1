5a2059f998c559ba97888517013f37807655962b
Keith Inscore <keithinscore@gmail.com>
2021-12-29 16:58:08 -0600

    finish ex16

--- src/main/java/com/xpanxion/java/springboot/da1/demo/repository/instructor/student3/GymRepository3.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/repository/instructor/student3/GymRepository3.java
@@ -4,8 +4,12 @@ import com.xpanxion.java.springboot.da1.demo.model.student3.Gym3;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GymRepository3 extends JpaRepository<Gym3, Integer>{
+
+    List<Gym3> findByNameContaining(String name);
 
 
 }
--- src/main/java/com/xpanxion/java/springboot/da1/demo/service/student3/GymService3.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/service/student3/GymService3.java
@@ -3,7 +3,12 @@ package com.xpanxion.java.springboot.da1.demo.service.student3;
 import com.xpanxion.java.springboot.da1.demo.model.student3.Gym3;
 import com.xpanxion.java.springboot.da1.demo.repository.instructor.student3.GymRepository3;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class GymService3 {
@@ -13,5 +18,9 @@ public class GymService3 {
 
     public Gym3 addGym(Gym3 gym){
         return gymRepository.save(gym);
+    }
+
+    public List<Gym3> getGym(String name){
+        return gymRepository.findByNameContaining(name);
     }
 }
