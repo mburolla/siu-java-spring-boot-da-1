045b3a868084281a1b25f4d6fcf1b64080c90180
EMANUEL GARCIA <e0g0186@HOMEOFFICE.wal-mart.com>
2021-12-29 16:24:07 -0600

    Finish ex16

--- src/main/java/com/xpanxion/java/springboot/da1/demo/service/student9/GymService9.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/service/student9/GymService9.java
@@ -1,12 +1,13 @@
 package com.xpanxion.java.springboot.da1.demo.service.student9;
 
 import com.xpanxion.java.springboot.da1.demo.model.student9.Gym9;
-import com.xpanxion.java.springboot.da1.demo.repository.instructor.GymRepository9;
+import com.xpanxion.java.springboot.da1.demo.repository.student9.GymRepository9;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Service
 public class GymService9 {
@@ -23,4 +24,9 @@ public class GymService9 {
     public Gym9 addGym(Gym9 gym){
         return gymRepository9.save(gym);
     }
+
+    public List<Gym9> getFindByNameContaining(String name){
+        return gymRepository9.findByNameContaining(name);
+    }
+
 }
