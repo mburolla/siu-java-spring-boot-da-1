81e582c506e8fb26b71180b8cdf8a21b261e17e8
Austin King <austintriesemail@gmail.com>
2021-12-29 14:09:08 -0600

    Create Gym Endpoint

---
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/service/student7/GymService7.java
@@ -0,0 +1,23 @@
+package com.xpanxion.java.springboot.da1.demo.service.student7;
+
+import com.xpanxion.java.springboot.da1.demo.model.student7.Gym;
+import com.xpanxion.java.springboot.da1.demo.repository.instructor.student7.GymRepository7;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Service
+public class GymService7 {
+    @PersistenceContext
+    public EntityManager entityManager;
+
+    @Autowired
+    private GymRepository7 gymRepository;
+
+    public Gym addGym(Gym gym) {
+        return gymRepository.save(gym);
+
+    }
+}
