475d75a57face8d107fd27c26bb85dff8db03c6d
Keith Inscore <keithinscore@gmail.com>
2021-12-30 10:38:23 -0600

    finish ex17

--- src/main/java/com/xpanxion/java/springboot/da1/demo/service/student3/GymService3.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/service/student3/GymService3.java
@@ -1,10 +1,10 @@
 package com.xpanxion.java.springboot.da1.demo.service.student3;
 
 import com.xpanxion.java.springboot.da1.demo.model.student3.Gym3;
+import com.xpanxion.java.springboot.da1.demo.model.student3.Member3;
 import com.xpanxion.java.springboot.da1.demo.repository.student3.GymRepository3;
+import com.xpanxion.java.springboot.da1.demo.repository.student3.MemberRepository3;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -16,6 +16,9 @@ public class GymService3 {
     @Autowired
     private GymRepository3 gymRepository;
 
+    @Autowired
+    private MemberRepository3 memberRepository;
+
     public Gym3 addGym(Gym3 gym){
         return gymRepository.save(gym);
     }
@@ -23,4 +26,6 @@ public class GymService3 {
     public List<Gym3> getGym(String name){
         return gymRepository.findByNameContaining(name);
     }
+
+    public Member3 addMember(Member3 member) {return memberRepository.save(member);}
 }
