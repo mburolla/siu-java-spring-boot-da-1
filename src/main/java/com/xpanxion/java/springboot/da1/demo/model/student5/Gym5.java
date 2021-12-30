905822d3a0873c7fcb82050e29feb5590852751f
Aaron Weegens <Aaron.Weegens@walmart.com>
2021-12-30 08:15:02 -0600

    Remove use of lombok to not alter pom.xml

--- src/main/java/com/xpanxion/java/springboot/da1/demo/model/student5/Gym5.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/model/student5/Gym5.java
@@ -1,17 +1,10 @@
 package com.xpanxion.java.springboot.da1.demo.model.student5;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @Table(name="gym5")
 public class Gym5 {
 
@@ -23,4 +16,93 @@ public class Gym5 {
     private String city;
     private String state;
     private String zip;
+
+    public Gym5() {
+        this.id = -1l;
+        this.name = "";
+        this.street1 = "";
+        this.street2 = "";
+        this.city = "";
+        this.state = "";
+        this.zip = "";
+    }
+
+    public Gym5(Long id, String name, String street1, String street2, String city, String state, String zip) {
+        this.id = id;
+        this.name = name;
+        this.street1 = street1;
+        this.street2 = street2;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet1() {
+        return street1;
+    }
+
+    public void setStreet1(String street1) {
+        this.street1 = street1;
+    }
+
+    public String getStreet2() {
+        return street2;
+    }
+
+    public void setStreet2(String street2) {
+        this.street2 = street2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Gym5{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", street1='" + street1 + '\'' +
+                ", street2='" + street2 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                '}';
+    }
 }
