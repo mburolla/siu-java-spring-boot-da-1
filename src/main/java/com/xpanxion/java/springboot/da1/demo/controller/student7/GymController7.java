764d79cfa12b4beee0e44c0234dcc1daadbeb651
Austin King <austintriesemail@gmail.com>
2021-12-30 13:57:34 -0600

    Create Find Gym by Name Functionality

--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student7/GymController7.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student7/GymController7.java
@@ -3,9 +3,9 @@ package com.xpanxion.java.springboot.da1.demo.controller.student7;
 import com.xpanxion.java.springboot.da1.demo.model.student7.Gym7;
 import com.xpanxion.java.springboot.da1.demo.service.student7.GymService7;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class GymController7 {
@@ -16,5 +16,10 @@ public class GymController7 {
     @PostMapping("student7/api/v1/gym")
     public Gym7 addGym(@RequestBody Gym7 gym) {
         return gymService.addGym(gym);
+    }
+
+    @GetMapping("student7/api/v1/gym") //?name={partialGymName}
+    public List<Gym7> findGym(@RequestParam String name) {
+        return gymService.findGym(name);
     }
 }
--- src/main/java/com/xpanxion/java/springboot/da1/demo/repository/student7/GymRepository7.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/repository/student7/GymRepository7.java
@@ -1,10 +1,14 @@
-package com.xpanxion.java.springboot.da1.demo.repository.instructor.student7;
+package com.xpanxion.java.springboot.da1.demo.repository.student7;
 
 import com.xpanxion.java.springboot.da1.demo.model.student7.Gym7;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GymRepository7 extends JpaRepository<Gym7, Integer> {
+
+    List<Gym7> findByNameContaining(String name);
 }
