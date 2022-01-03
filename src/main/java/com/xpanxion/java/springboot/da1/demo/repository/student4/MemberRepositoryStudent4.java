4781879f2f3987adb28c0331274b30f4aeaed44b
Elizabeth Michael <lisakada@gmail.com>
2022-01-03 16:41:24 -0600

    Finish Ex.18

--- src/main/java/com/xpanxion/java/springboot/da1/demo/repository/student4/MemberRepositoryStudent4.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/repository/student4/MemberRepositoryStudent4.java
@@ -1,6 +1,5 @@
 package com.xpanxion.java.springboot.da1.demo.repository.student4;
 
-
 import com.xpanxion.java.springboot.da1.demo.model.student4.Member4;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
