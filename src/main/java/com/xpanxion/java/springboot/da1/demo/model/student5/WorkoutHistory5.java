85f2d2a22bf25f561df45fd23bdfe050e5c61f36
Aaron Weegens <Aaron.Weegens@walmart.com>
2022-01-03 09:44:16 -0600

    return 404 if member not found

--- src/main/java/com/xpanxion/java/springboot/da1/demo/model/student5/WorkoutHistory5.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/model/student5/WorkoutHistory5.java
@@ -1,7 +1,6 @@
 package com.xpanxion.java.springboot.da1.demo.model.student5;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
--- src/main/java/com/xpanxion/java/springboot/da1/demo/service/student5/WorkoutHistoryService5.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/service/student5/WorkoutHistoryService5.java
@@ -3,11 +3,10 @@ package com.xpanxion.java.springboot.da1.demo.service.student5;
 import com.xpanxion.java.springboot.da1.demo.model.student5.WorkoutHistory5;
 import com.xpanxion.java.springboot.da1.demo.repository.student5.WorkoutHistoryRepository5;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Service
@@ -20,13 +19,13 @@ public class WorkoutHistoryService5 {
         return workoutHistoryRepository.save(new WorkoutHistory5(memberId, time));
     }
 
-    public WorkoutHistory5 workoutCheckOut(Long memberId, Date time) throws ParseException {
-        var workout = workoutHistoryRepository.findAllByMemberIdOrderByWorkoutIdDesc(memberId)
-                .stream()
-                .toList()
-                .get(0);
+    public WorkoutHistory5 workoutCheckOut(Long memberId, Date time) {
+        var workoutList = workoutHistoryRepository.findAllByMemberIdOrderByWorkoutIdDesc(memberId);
+        if (workoutList.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        var workout = workoutList.get(0);
         workout.setCheckOut(time);
         workoutHistoryRepository.save(workout);
+
         return workout;
     }
 }
