c783b9ae40629cbdc08038b89f675befa0d1d58c
Elizabeth Michael <lisakada@gmail.com>
2022-01-03 16:39:33 -0600

    Finish Ex.18

--- src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student4/MemberController4.java
+++ src/main/java/com/xpanxion/java/springboot/da1/demo/controller/student4/MemberController4.java
@@ -1,12 +1,10 @@
 package com.xpanxion.java.springboot.da1.demo.controller.student4;
 
 import com.xpanxion.java.springboot.da1.demo.model.student4.Member4;
+import com.xpanxion.java.springboot.da1.demo.model.student4.Workout4;
 import com.xpanxion.java.springboot.da1.demo.service.student4.MemberServiceStudent4;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MemberController4 {
@@ -18,5 +16,15 @@ public class MemberController4 {
     public Member4 addMember(@PathVariable("gymId") Long gymId, @RequestBody Member4 member4) {
         member4.setGymId(gymId);
         return memberService.addMember(member4);
+    }
+
+    @PostMapping("student4/api/v1/member/{memberId}/checkin")
+    public Workout4 checkIn(@PathVariable int memberId, @RequestParam String time) {
+        return memberService.checkIn(memberId, time);
+    }
+
+    @PostMapping("student4/api/v1/member/{memberId}/checkout")
+    public Workout4 checkOut(@PathVariable int memberId, @RequestParam String time) {
+        return memberService.checkOut(memberId, time);
     }
 }
